04392dcc884e5a1123fe1c13b8fdc5e32f3cb3e1
Ali Mirlou <alimirlou@gmail.com>
2024-12-12 20:04:14 +0330

    Update migrations

---
+++ lib/db/migrations/0004_windy_black_bird.sql
@@ -0,0 +1,6 @@
+ALTER TABLE "Message" ALTER COLUMN "content" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "Message" ADD COLUMN "images" json;--> statement-breakpoint
+ALTER TABLE "Message" ADD COLUMN "audios" json;--> statement-breakpoint
+ALTER TABLE "Message" ADD COLUMN "videos" json;--> statement-breakpoint
+ALTER TABLE "Message" ADD COLUMN "voice" json;--> statement-breakpoint
+ALTER TABLE "Message" ADD COLUMN "model" varchar;
