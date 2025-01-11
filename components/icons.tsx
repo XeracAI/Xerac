e0081c00efc2f582529372ed90538e3f0514d43c
Ali Mirlou <alimirlou@gmail.com>
2025-01-11 20:50:46 +0330

    Add some icons

--- components/icons.tsx
+++ components/icons.tsx
@@ -417,6 +417,44 @@ export const PencilEditIcon = ({ size = 16 }: { size?: number }) => {
   );
 };
 
+export const LeftArrowIcon = ({ size = 16 }: { size?: number }) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      style={{ color: 'currentcolor' }}
+      className="icon-md-heavy"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M14.7071 5.29289C15.0976 5.68342 15.0976 6.31658 14.7071 6.70711L9.41421 12L14.7071 17.2929C15.0976 17.6834 15.0976 18.3166 14.7071 18.7071C14.3166 19.0976 13.6834 19.0976 13.2929 18.7071L7.29289 12.7071C7.10536 12.5196 7 12.2652 7 12C7 11.7348 7.10536 11.4804 7.29289 11.2929L13.2929 5.29289C13.6834 4.90237 14.3166 4.90237 14.7071 5.29289Z"
+        fill="currentColor"
+      />
+    </svg>
+  )
+}
+
+export const RightArrowIcon = ({ size = 16 }: { size?: number }) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      style={{ color: 'currentcolor' }}
+      className="icon-md-heavy"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M9.29289 18.7071C8.90237 18.3166 8.90237 17.6834 9.29289 17.2929L14.5858 12L9.29289 6.70711C8.90237 6.31658 8.90237 5.68342 9.29289 5.29289C9.68342 4.90237 10.3166 4.90237 10.7071 5.29289L16.7071 11.2929C16.8946 11.4804 17 11.7348 17 12C17 12.2652 16.8946 12.5196 16.7071 12.7071L10.7071 18.7071C10.3166 19.0976 9.68342 19.0976 9.29289 18.7071Z"
+        fill="currentColor"
+      />
+    </svg>
+  )
+}
+
 export const CheckedSquare = ({ size = 16 }: { size?: number }) => {
   return (
     <svg
@@ -786,7 +824,7 @@ export const ThumbDownIcon = ({ size = 16 }: { size?: number }) => (
     strokeLinejoin="round"
     viewBox="0 0 16 16"
     width={size}
-    style={{ color: 'currentcolor' }}
+    style={{ color: 'currentcolor', transform: 'scale(-1,1)' }}
   >
     <path
       fillRule="evenodd"
