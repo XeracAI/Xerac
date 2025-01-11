6bb173ea5fa20ec4152795fc01b61d48fe033829
Ali Mirlou <alimirlou@gmail.com>
2025-01-11 20:46:46 +0330

    Update toast font family

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,7 +64,7 @@ export default async function RootLayout({
           defaultTheme="system"
           enableSystem
         >
-          <Toaster position="top-center" />
+          <Toaster position="top-center" toastOptions={{ style: { fontFamily: "Shabnam, ui-sans-serif, system-ui, -apple-system" } }} />
           {children}
         </ThemeProvider>
       </body>
